5564755bac00089742f3c074d6bf27c0e65cd6a3
Rushane Barnes <barnesrushane@hotmail.com>
2018-11-11 02:54:00 +0000

    Exercise 4

--- process.js
+++ process.js
@@ -1,23 +1,39 @@
-  function defineWord() {
-    let word = document.getElementById("word").value;
+/* global $ */
+$(document).ready(function(){
+    $("#singledef").on('click', function(){
+       let word = $("#word").val();
+       let link = "https://info2180-lab6-rushane.c9users.io/request.php?q="+ word;
+       
+       $.ajax(link, {
+           method: 'GET',
+       }).done(function(res){
+           $("#result").html(res);
+       }).fail(function(err){
+           $("#result").html(err);
+       });
+    });
     
-      /* global fetch */
-      /*fetch('request.php?q=definition')
-      .then(response => response.text())
-      .then(data => {
-  
-      alert(data)
-      }); */
-      var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("result").innerHTML =
-        this.responseText;
-      }
-    };
-    xhttp.open("GET", "request.php?q=" + word, true);
-    xhttp.send();
-      
-  }
-
-  
+    $("#all").on('click', function(){
+        let link = "https://info2180-lab6-rushane.c9users.io/request.php?q=&all=true";
+        
+        $.ajax(link, {
+           method: 'GET',
+           dataType: "xml",
+       }).done(function(res){
+           let defi = $(res).find('definition');
+           
+           $("#result").html(""); //clear if previous result given
+           $("#result").append('<ol></ol>');
+           
+           $(defi).each(function(){
+               let ret = '<h3>'+$(this).attr('name')+'</h3>';
+               ret += '<p>'+$(this).text()+'</p>';
+               ret += '<p> -'+$(this).attr('author')+'</p>';
+               $("#result ol").append('<li>'+ret+'</li>');
+           });
+       }).fail(function(err){
+           $("#result").html(err);
+       });
+    });
+    
+});
